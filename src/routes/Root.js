0508b5906500cbaa8e220f39d0eed8147c0c7464
Emanuel <emanuel.duca2000@gmail.com>
2024-04-11 10:06:50 +0200

    Changes related with header

--- src/routes/Root.js
+++ src/routes/Root.js
@@ -1,13 +1,26 @@
 import { Link, Outlet } from "react-router-dom"
+import pokemonIcon from "../images/pokemon-icon.png"
+
 
 export default function App() {
     return (
         <>
-            <nav>
-            <Link to="/">Home </Link>
-            <Link to="/about">About </Link>
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <div className="container">
+                    <Link className="navbar-brand" to="/"><img src={pokemonIcon} height="30px" /> PokemonDex</Link>
+                    <div className="collapse navbar-collapse">
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/">Home</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/about">About</Link>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
             </nav>
-            <Outlet />
+            <Outlet /> 
         </>
     )
 }
